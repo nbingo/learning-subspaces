12495a964fc94c64bef898ddee048db661722fe2
nbingo <5738735+nbingo@users.noreply.github.com>
2021-11-14 22:33:01 -0800

    Some more tests to make sure we're properly outputting the state_dict of the underlying model at a specific alpha.

--- sub_wrap_test.py
+++ sub_wrap_test.py
@@ -13,6 +13,11 @@ ln = nn.Linear(5, 5)
 
 incompatible_keys = ls.load_state_dict(ln.state_dict())
 
+num_vertices = 3
+alpha = torch.full((num_vertices,), np.sqrt(num_vertices) / num_vertices)
+
+print(ls.state_dict_at_alpha(alpha).keys())
+
 SubResNet = to_subspace_class(models.ResNet, verbose=True)
 sub_resnet18 = SubResNet(models.resnet.BasicBlock, [2, 2, 2, 2])
 resnet18 = models.resnet18()
@@ -20,3 +25,6 @@ resnet18 = models.resnet18()
 incompatible_keys = sub_resnet18.load_state_dict(resnet18.state_dict())
 
 print(incompatible_keys)
+num_vertices = 2
+alpha = torch.full((num_vertices,), np.sqrt(num_vertices) / num_vertices)
+print(sub_resnet18.state_dict_at_alpha(alpha).keys())
